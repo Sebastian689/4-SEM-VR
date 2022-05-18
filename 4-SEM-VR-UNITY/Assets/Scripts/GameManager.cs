2312ac22e9f22a581ec113633adbfabeafcc3084
Sebastian <sechr17@student.sdu.dk>
2022-05-18 23:26:13 +0200

    UV printer done

--- 4-SEM-VR-UNITY/Assets/Scripts/GameManager.cs
+++ 4-SEM-VR-UNITY/Assets/Scripts/GameManager.cs
@@ -3,27 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {   
     GameObject _text;
+    public float _currentTime;
+    bool _timerActive = false;
+
     public TextMeshProUGUI _setText;
+
+    public GameObject UncutPCB;
+
+    public GameObject PCBPaper;
+
+    public GameObject Drawer;
+    Vector3 _drawerPos;
+
+
 
     public bool labcoatEquipped;
     public bool glassesEquipped;
     public bool glovesEquipped;
+
+    public bool patternApplied;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _text = GameObject.FindGameObjectWithTag("Text");
         _setText = _text.GetComponent<TextMeshProUGUI>();
         _text.SetActive(false);
+
+        _currentTime = 0;
+
+        _drawerPos = Drawer.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (_timerActive == true) {
+            _currentTime = _currentTime + Time.deltaTime;
+            TimeSpan time = TimeSpan.FromSeconds(_currentTime);
+            _text.SetActive(true);
+            _setText.text = time.ToString(@"mm\:ss\:fff");
+
+            if (_currentTime > 85) {
+                StopTimer();
+                _text.SetActive(true);
+                _setText.text = "The PCB has been in the UV printer for too long, and is ruined.";
+                _currentTime = 0;
+                Restart();
+
+            }
+        }
         
+    }
+
+    void StartTimer() {
+        Debug.Log("Timer started");
+        _timerActive = true;
+    }
+
+    void StopTimer() {
+        Debug.Log("Timer stopped");
+        _timerActive = false;
+        _text.SetActive(false);
     }
 
     void Labcoat() {
@@ -40,6 +89,7 @@ public class GameManager : MonoBehaviour
             _setText.SetText("Lab coat equipped!");
         }
 
+        StopAllCoroutines();
         StartCoroutine("StopText");
     }
 
@@ -56,6 +106,7 @@ public class GameManager : MonoBehaviour
             _setText.text = "Safety glasses equipped!";
         }
 
+        StopAllCoroutines();
         StartCoroutine("StopText");
     }
 
@@ -72,10 +123,32 @@ public class GameManager : MonoBehaviour
             _setText.text = "Safety gloves equipped!";
         }
 
+        StopAllCoroutines();
+        StartCoroutine("StopText");
+    }
+
+    void ApplyPattern() {
+        patternApplied = true;
+        _setText.text = "Pattern has been applied to the PCB";
+
+        StopAllCoroutines();
+        StartCoroutine("StopText");
+    }
+
+    void Restart() {
+        
+        Drawer.transform.position = _drawerPos;
+
+        Instantiate(UncutPCB, new Vector3(0.243049994f,0.899500012f,-1.54264998f), Quaternion.identity);
+
+        Instantiate(PCBPaper, new Vector3(-0.627099991f,0.333999991f,-3.51789999f), Quaternion.identity);
+
+        StopAllCoroutines();
         StartCoroutine("StopText");
     }
 
     IEnumerator StopText() {
+        
         yield return new WaitForSeconds(3);
         _text.SetActive(false);
     }
